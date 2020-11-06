7da77bfe635bd55b61ae196c1bc88bb20eff4bfa
luvlysehgal <luvly.sehgal814@gmail.com>
2020-11-06 15:34:19 +0530

    Modifying file CompanyEmpWage to final solution

--- CompanyEmpWage.java
+++ CompanyEmpWage.java
@@ -3,12 +3,21 @@ public class CompanyEmpWage {
 	public final int empRatePerHr;
 	public final int numOfWorkingDays;
 	public final int maxHrPerMonth;
+	public int totalEmpWage;
 
 	public CompanyEmpWage(String company, int empRatePerHr, int numOfWorkingDays, int maxHrPerMonth) {
 		this.company = company;
 		this.empRatePerHr = empRatePerHr;
 		this.numOfWorkingDays = numOfWorkingDays;
 		this.maxHrPerMonth = maxHrPerMonth;
+		totalEmpWage = 0;
+	}
+
+	public void setTotalEmpWage(int totalEmpWage) {
+		this.totalEmpWage = totalEmpWage;
+	}
+	public String toString(){
+		return "Total Emp Wage for company: " +company+ " is " +totalEmpWage;
 	}
 }
 
